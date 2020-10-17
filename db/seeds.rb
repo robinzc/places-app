4be8eac1cc9097437d3ce85c7c4df26494721f31
robin <robin.j.soto@gmail.com>
2020-10-16 17:33:36 -0700

    creates app and adds REST actions

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Place.create!(name: "Sears Tower", address: "233 South Wacker Drive Chicago, IL 60606")
+Place.create!(name: "Merchandise Mart", address: "222 W Merchandise Mart Plaza, Chicago, IL 60654")
+Place.create!(name: "John Hancock Center", address: "875 N Michigan Ave, Chicago, IL 60611")
